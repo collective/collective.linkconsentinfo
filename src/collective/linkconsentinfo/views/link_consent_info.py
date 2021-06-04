213482ad9adc9c16716e3faad7ce96f2dc668fa4
MrTango <md@derico.de>
2021-06-04 21:13:13 +0300

    override link_redirect_view

--- src/collective/linkconsentinfo/views/link_consent_info.py
+++ src/collective/linkconsentinfo/views/link_consent_info.py
@@ -1,18 +1,53 @@
 # -*- coding: utf-8 -*-
 
 from collective.linkconsentinfo import _
+from plone.app.contenttypes.browser.link_redirect_view import (
+    LinkRedirectView,
+    NON_REDIRECTABLE_URL_SCHEMES,
+    NON_RESOLVABLE_URL_SCHEMES,
+)
+from plone.registry.interfaces import IRegistry
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import ITypesSchema
 from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getUtility
+
+import six
 
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+class LinkConsentInfo(LinkRedirectView):
+    """ Subclass LinkRedirectView to inject link consent switch and template
+    """
 
-
-class LinkConsentInfo(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('link_consent_info.pt')
+    index = ViewPageTemplateFile("link_consent_info.pt")
 
     def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+        """Redirect to the Link target URL, if and only if:
+         - redirect_links property is enabled in
+           configuration registry
+         - the link is of a redirectable type (no mailto:, etc)
+         - enable_consent_info is False
+         - AND current user doesn't have permission to edit the Link"""
+
+        context = self.context
+        mtool = getToolByName(context, "portal_membership")
+
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ITypesSchema, prefix="plone")
+        redirect_links = settings.redirect_links
+
+        can_edit = mtool.checkPermission("Modify portal content", context)
+        redirect_links = (
+            redirect_links
+            and not context.enable_consent_info
+            and not self._url_uses_scheme(NON_REDIRECTABLE_URL_SCHEMES)
+        )
+
+        if redirect_links and not can_edit:
+            target_url = self.absolute_target_url()
+            if six.PY2:
+                target_url = target_url.encode("utf-8")
+            return self.request.RESPONSE.redirect(target_url)
+        else:
+            return self.index()

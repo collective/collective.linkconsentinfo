780c573d9e53bf99117248eedf7ca5e62a497670
MrTango <md@derico.de>
2021-06-07 11:48:45 +0300

    update python classifiers

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the collective.linkconsentinfo package."""
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 long_description = '\n\n'.join([
@@ -23,8 +22,10 @@ setup(
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
         "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
